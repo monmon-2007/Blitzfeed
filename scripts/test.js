e9664d4231d331c7cb09abe455a162052abc8d3c
Jonathan Tan <jmtan188@gmail.com>
2015-02-28 21:09:56 -0500

    Fixed speed changing.  Cleaned GUI.

--- scripts/test.js
+++ scripts/test.js
@@ -6,7 +6,7 @@ var right = document.getElementById("rightWord");
 var left = document.getElementById("leftWord");
 var address = document.getElementById("currentWord");
 
-var article = "Nueva York se prepara para la llegada este domingo de un nuevo temporal de nieve, después de registrar el mes de febrero más frío en décadas, según los expertos. Aunque no se esperan grandes precipitaciones, las autoridades de la ciudad anunciaron hoy una alerta y han comenzado a diseminar sal por las calles, que en muchas zonas de la Gran Manzana están aún cubiertas por nieve y hielo de tormentas anteriores. Según el Servicio Nacional de Meteorología, durante febrero la temperatura media en Nueva York ha sido de 24 grados, claramente por debajo de lo habitual. Los expertos esperan que el mes cierre como el febrero más frío en la ciudad desde 1934 y a lo largo de las últimas semanas se han registrado algunas de las temperaturas más bajas en décadas, cuyo impacto se ha multiplicado además por fuertes rachas de viento. A finales de enero, uno de los temporales invernales paralizó durante casi dos días Nueva York, obligando a cerrar el transporte público, a pesar de que finalmente la tormenta no fue todo lo fuerte que se esperaba.";
+var article = "Nueva York se prepara para la llegada este domingo de un nuevo temporal de nieve, después de registrar el mes de febrero más frío en décadas, según los expertos. Aunque no se esperan grandes precipitaciones, las autoridades de la ciudad anunciaron hoy una alerta y han comenzado a diseminar sal por las calles, que en muchas zonas de la Gran Manzana están aún cubiertas por nieve y hielo de tormentas anteriores. Según el Servicio Nacional de Meteorología, durante febrero la temperatura media en Nueva York ha sido de 24 grados, claramente por debajo de lo habitual. Los expertos esperan que el mes cierre como el febrero más frío en la ciudad desde 1934 y a lo largo de las últimas semanas se han registrado algunas de las temperaturas más bajas en décadas, cuyo impacto se ha multiplicado además por fuertes rachas de viento. A finales de enero, uno de los temporales invernales paralizó durante casi dos días Nueva York, obligando a cerrar el tr";
 
 var articleArray = article.split(" ");
 
@@ -19,15 +19,28 @@ console.log(wpm);
 displayArticle = function () {
 	if(articleArray[number] == null){
 		clearInterval(timer);
-		
 		address.innerHTML = "Next >";
-		
+		right.innerHTML = "...";
+		left.innerHTML = "...";
 		return;
 	}
-		
-	left.innerHTML = articleArray[number - 1];
+	var leftWord;
+	
+		if(articleArray[number - 1] == null)
+			leftWord = "...";
+		else
+			leftWord = articleArray[number - 1];
+	
+	left.innerHTML = leftWord;
 	address.innerHTML = articleArray[number];
-	right.innerHTML = articleArray[number + 1];
+	
+	var rightWord;
+	
+		if(articleArray[number + 1] == null)
+			rightWord = "...";
+		else
+			rightWord = articleArray[number + 1];
+	right.innerHTML = rightWord;
 	++number;
 }
 
@@ -38,25 +51,21 @@ displayArticle = function () {
 	var y = document.getElementById("myList").options;
 	index = y[x].index;
 	if (index == 0){
+		setWPM(60);
         return;
     }
     else if (index == 1){
-		console.log("1");
-		setWPM(60);
+		setWPM(200);
         return;
     }
     else if (index == 2){
 		console.log("2");
-		setWPM(200);
-		return;
-    }
-    else if (index == 3){
 		setWPM(350);
 		return;
     }
-    else if (index == 4){
+    else if (index == 3){
 		setWPM(500);
-        return;
+		return;
     }
 }
 
@@ -67,6 +76,6 @@ function setWPM(wpmArg){
 	console.log(milliseconds);
 	
 	clearInterval(timer);
-	setInterval(function(){displayArticle()}, milliseconds);
+	timer = setInterval(function(){displayArticle()}, milliseconds);
 };
 	
