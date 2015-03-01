4a17bf3705f40e11ec006c42dd10be4f72b38800
root <root@debian>
2015-03-01 08:54:44 +0000

    Language select works.

--- app.py
+++ app.py
@@ -5,41 +5,49 @@ import string
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def showNews():
-	link = 'http://api.feedzilla.com/v1/categories/26/articles.json'
-	
-	if request.method == 'POST':
-		if lang == 'en':
-			link = 'http://api.feedzilla.com/v1/categories/26/articles.json'
+def home():
+    if request.method == 'POST':
+        lang = request.form['lang']
+        if lang == 'en':
+            link = 'http://api.feedzilla.com/v1/categories/26/articles.json'
 		
-		elif lang == 'pl':
-			link = 'http://api.feedzilla.com/v1/categories/170/articles.json'
+        elif lang  == 'pl':
+            link = 'http://api.feedzilla.com/v1/categories/170/articles.json'
 		
-		elif lang == 'es':
-			link = 'http://api.feedzilla.com/v1/categories/115/articles.json'
+        elif lang == 'es':
+            link = 'http://api.feedzilla.com/v1/categories/115/articles.json'
 			
-		elif lang == 'tr':
-			link = 'http://api.feedzilla.com/v1/categories/70/articles.json'
+        elif lang == 'tr':
+            link = 'http://api.feedzilla.com/v1/categories/70/articles.json'
 
-	r = requests.get(link)
-	
+        final_list = showNews(link)
+
+        return render_template('index.html', news=final_list)
+
+    else:
+        
+        final_list = showNews('http://api.feedzilla.com/v1/categories/26/articles.json') 
+        return render_template('index.html', news=final_list)
+
+
+def showNews(link): 
+    r = requests.get(link)
+    
     raw_list = [] 
     final_list = [] 
     i = 0 
-
+    
     for article in r.json()['articles']:
         raw_list.append(article['summary'].split())
-    
+        
     while len(raw_list) > i: 
         for word in raw_list[i]:
-            final_list.append(word)
+            final_list.append(word) 
         
-        i+= 1
-			
-
-    return render_template('index.html', news=final_list)
-
-
+        i+= 1 
+    
+    return final_list 
+      
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80)
 
